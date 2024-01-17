30ba3dd49b60dad66d1ef2b1bd5d939e059e49be
shree-2802 <pattupadmashree@gmail.com>
2024-01-17 22:10:42 +0530

    header constants

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,7 +1,19 @@
-import {gericht} from './images'
+import { gericht, spoon, welcome } from './images';
 
 //NavBar Constants
 export const navBarConstants = {
   image: gericht,
   navLinks: ['Home', 'Pages', 'Contact Us', 'Blog', 'Landing'],
+};
+
+//Header Constants
+
+export const headerConstants = {
+  chase: 'Chase the new Flavour',
+  mainHead: 'The key to Fine dining',
+  spoon: spoon,
+  subText:
+    'Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus',
+  buttonContent: 'Explore Menu',
+  welcome: welcome,
 };
