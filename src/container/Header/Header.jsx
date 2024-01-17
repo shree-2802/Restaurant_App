a2199d559b47a08a1c68d97847ec5c7ee08d34ad
shree-2802 <pattupadmashree@gmail.com>
2024-01-17 22:20:28 +0530

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { headerConstants } from '../../constants/constants';
 import './Header.css';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+const Header = () => {
+  return (
+    <div className='app__header'>
+      <div className='app__header_left'>
+        <p>{headerConstants.chase}</p>
+        <img src={headerConstants.spoon} alt='->' />
+        <h1>{headerConstants.mainHead}</h1>
+        <p>{headerConstants.subText}</p>
+        <button>{headerConstants.buttonContent}</button>
+      </div>
+      <div className='app__header_right'>
+        <img src={headerConstants.welcome} alt='Welcome' />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
