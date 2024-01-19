04a61a5f72ac8e512967451fa983be6dcef42603
shree-2802 <pattupadmashree@gmail.com>
2024-01-19 11:56:30 +0530

    jsx

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
 import './FooterOverlay.css';
 
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { newsletterConstants } from '../../constants/constants';
+import Button from '../Button/button';
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter--text'>
+      <SubHeading text={newsletterConstants.text} />
+      <h1>{newsletterConstants.title}</h1>
+      <p>{newsletterConstants.subText}</p>
+    </div>
+    <div className='app__newsletter--input'>
+      <input type='email' placeholder={newsletterConstants.placeholder} />
+      <Button text={newsletterConstants.buttonContent} />
+    </div>
   </div>
 );
 
